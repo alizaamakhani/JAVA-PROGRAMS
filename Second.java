6ffbdbbd1fc2bc33569368ece4b7123846de0237
Aliza Makhani <makhanializa98@gmail.com>
2026-07-25 09:26:48 +0530

    Add first Java programs

---
+++ Second.java
@@ -0,0 +1,13 @@
+class Second{
+    int a=10; //instance variable
+    static double b = 10.5;//static variable
+    public static void main(String args[]){
+        boolean c=true;//local variable
+        Second s= new Second();
+        System.out.println(s.a +" "+ b+" "+c+ " ");
+        
+
+
+
+    }
+}
