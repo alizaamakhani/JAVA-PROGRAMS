1f163abac50fcde7be376836602383bdf2e2ad06
Aliza Makhani <makhanializa98@gmail.com>
2026-07-26 15:06:10 +0530

    Delete InstanceBlock.java

--- InstanceBlock.java
+++
@@ -1,9 +0,0 @@
-class InstanceBlock{
-    //instance block not use any keywords.....
-    {
-        System.out.println("THIS IS AN INSTANCE BLOCK!!");
-    }
-    public static void main(String args[]){
-        InstanceBlock i = new InstanceBlock();
-    }
-}
